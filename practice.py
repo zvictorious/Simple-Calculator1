89fbc656dff24a9c4e424c9aa37048a7585c5313
zvictorious <121365726+zvictorious@users.noreply.github.com>
2022-12-24 20:53:45 -0500

    Delete practice.py

--- practice.py
+++
@@ -1,9 +0,0 @@
-name = input("Hello, what is your name? ")
-print("That's dope, nice to meet you", name, "!")
-good_day = input("Did you have a good day ? ")
-
-if good_day == "yes":
-    print("we love to see it !")
-else:
-    print("i hope you have a better day tomorrow!")
-print ("See you around", name, "!")
